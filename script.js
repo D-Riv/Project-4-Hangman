f08d8168d3cd72c9a4c7af908503d25784a9d69f
Dennis Rivera <rivd19@outlook.com>
2020-02-11 17:41:14 -0500

    Updated keypad toggle button and corrected errors

--- script.js
+++ script.js
@@ -33,26 +33,32 @@ randomWord();
 let keypadInit = document.querySelector(".keypad-activator");
 let keypad = document.querySelector(".keypad");
 
-keypadInit.addEventListener("click", generateButtons);
 function generateButtons() {
   let buttons = "abcdefghijklmnopqrstuvwxyz".split("");
   for(let i = 0; i < buttons.length; i++) {
-    let newKeypad = document.createElement("button");
-    newKeypad.classList.add("letter");
-    newKeypad.innerHTML = buttons[i];
-    keypad.appendChild(newKeypad);
-  // newKeypad.appendChild(buttons[i]);
-  }
-  keypadInit.removeEventListener("click", generateButtons);
-
-  keypadInit.addEventListener("click", removeButtons);
-  function removeButtons() {
-    let newKeypad = document.querySelectorAll(".letter")
-    for(let i = 0; i < newKeypad.length; i++) {
-    keypad.removeChild(newKeypad[i]);
-    }
+    let newButton = document.createElement("button");
+    newButton.classList.add("letter");
+    newButton.innerHTML = buttons[i];
+    keypad.appendChild(newButton);
+  // newButton.appendChild(buttons[i]);
   }
 }
+generateButtons();
+
+keypadInit.addEventListener("click", generateDisplay);
+function generateDisplay() {
+  let keypad = document.querySelector(".keypad");
+
+  if(keypad.style.display === "none") {
+    keypad.style.display = "block";
+  }else {
+    keypad.style.display = "none";
+  }
+}
+
+
+
+
 
 
 //restart game
